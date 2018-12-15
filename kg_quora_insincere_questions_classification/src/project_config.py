486c962b4c3d35acf550245cd0c7edd6f78da488
BlackPoint-CX <chenxiang0815@gmail.com>
2018-12-15 21:37:49 +0800

    mbp

---
+++ kg_quora_insincere_questions_classification/src/project_config.py
@@ -0,0 +1,12 @@
+import os
+
+REPO_DIR = '/Users/alfredchen/Develop/PyRepos/'
+PROJECT_DIR = os.path.join(REPO_DIR,
+                           'kg_quora_insincere_questions_classification/kg_quora_insincere_questions_classification')
+DATA_DIR = os.path.join(PROJECT_DIR, 'data')
+SRC_DIR = os.path.join(PROJECT_DIR, 'src')
+
+SOURCE_DIR = os.path.join(DATA_DIR, 'source')
+EMBEDDING_DIR = os.path.join(DATA_DIR, 'embedding')
+
+
