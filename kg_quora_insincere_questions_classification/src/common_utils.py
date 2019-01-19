967f0f4ae0ba0bdd5b9a468b340b7a20cf0d5df2
BlackPoint <chenxiang0815@gmail.com>
2019-01-19 15:21:32 +0800

    modify on desktop

--- kg_quora_insincere_questions_classification/src/common_utils.py
+++ kg_quora_insincere_questions_classification/src/common_utils.py
@@ -9,12 +9,11 @@ elif platform.system() == 'Linux':
 else:
     raise ValueError('Wrong Project Directory')
 
-
-INPUT_DIR = os.path.join(PROJECT_DIR,'input')
-TRAIN_FILE_PATH = os.path.join(INPUT_DIR,'train.csv')
-TEST_FILE_PATH = os.path.join(INPUT_DIR,'test.csv')
-SAMPLE_SUBMISSION_FILE_PATH = os.path.join(INPUT_DIR,'sample_submission.csv')
-
+SRC_DIR = os.path.join(PROJECT_DIR, 'src')
+INPUT_DIR = os.path.join(SRC_DIR, 'input')
+TRAIN_FILE_PATH = os.path.join(INPUT_DIR, 'train.csv')
+TEST_FILE_PATH = os.path.join(INPUT_DIR, 'test.csv')
+SAMPLE_SUBMISSION_FILE_PATH = os.path.join(INPUT_DIR, 'sample_submission.csv')
 
 MAX_FEATURES = 10000
 MAX_LEN = 100
